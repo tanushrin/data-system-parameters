46bc4c8ff0d6148f4e32ab9ea6a720dbc97b266c
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-10 12:29:38 +0200

    System Parameters done.

--- calc.py
+++ calc.py
@@ -4,9 +4,14 @@ import sys
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    #pass  # YOUR CODE HERE
+    if sys.argv[2] == "+":
+        return int(sys.argv[1]) + int(sys.argv[3])
+    elif sys.argv[2] == "-":
+        return int(sys.argv[1]) - int(sys.argv[3])
+    else:
+        return int(sys.argv[1]) * int(sys.argv[3])
 
 
 if __name__ == "__main__":
     print(main())
-    
